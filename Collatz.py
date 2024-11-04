4348a5dba21c2dc088a15bbc5504015537fe69cc
Mohammed Faisal <mohammed.faisal337@gmail.com>
2024-11-04 18:52:33 +0000

    Update and rename trial.py to Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -1,5 +1,4 @@
-# Read the input. Don't change this line. 
-# No need to handle errors, only integers will be given.
+
 def Collatz(n):
     count = 0
     while n != 1:
@@ -13,8 +12,3 @@ def Collatz(n):
 n = int(input())
 r = Collatz(n)
 print(r)
-
-# Do your thing here...
-
-def middle(a,b,c):
-    el = [a,b,c]
